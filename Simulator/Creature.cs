47619ae678f340e66dff69890658b146aba42711
TomaszMielecki <tomek.mielecki@gmail.com>
2024-10-30 20:02:20 +0100

    creature push

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -8,5 +8,35 @@ namespace Simulator
 {
     internal class Creature
     {
+        private string _name = "Unkown";
+
+        private int _level = 1;
+
+        public string Name
+        {
+            get => name;
+            init
+            {
+                string trimname = value.Trim();
+                if (trimname.Length < 3)
+                {
+                    trimname = trimname.PadRight(3, '#');
+                }
+                if (trimname.Length > 25)
+                {
+                    trimname = trimname.Substring(0, 25).TrimEnd();
+                    if (trimname.Length < 3)
+                    {
+                        trimname = trimname.PadRight(3, '#');
+                    }
+                }
+                if (char.IsLower(trimname[0]))
+                {
+                    trimname = char.ToUpper(trimname[0]) + trimname.Substring(1);
+                }
+
+                _name = trimname;
+            }
+        }
     }
 }
