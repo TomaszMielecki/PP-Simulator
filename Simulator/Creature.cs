f8ce68ff9e6d3e87c8c97a477b8476457eb20d85
TomaszMielecki <tomek.mielecki@gmail.com>
2024-11-07 20:05:10 +0100

    Lab-4 spóźnione

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Simulator
 {
-    internal class Creature
+    public abstract class Creature
     {
         private string _name = "Unkown";
 
@@ -14,7 +15,7 @@ namespace Simulator
 
         public string Name
         {
-            get => name;
+            get => _name;
             init
             {
                 string trimname = value.Trim();
@@ -37,6 +38,59 @@ namespace Simulator
 
                 _name = trimname;
             }
+
+        }
+        public int Level
+        {
+            get => _level;
+            init 
+            {
+                _level = Validator.Limiter(value, 1, 10);
+            }
+        }
+        public Creature()
+        {
+        }
+        public Creature(string name, int level = 1)
+        {
+            Name = name;
+            Level = level;
+        }
+        public abstract string Info { get; }
+        public abstract int Power { get; }
+        public abstract void SayHi();
+        public override string ToString()
+        {
+            return $"{GetType().Name.ToUpper()}: {Info}";
+        }
+
+
+        public void Upgrade()
+        {
+            if (_level < 10)
+            {
+                _level++;
+            }
+        }
+
+        public void Go(Direction direction)
+        {
+            string lowerdirection = direction.ToString().ToLower();
+            Console.WriteLine($"{Name} goes {lowerdirection}.");
+        }
+
+        public void Go(Direction[] directions)
+        {
+            foreach (Direction direction in directions)
+            {
+                Go(direction);
+            }
+        }
+
+        public void Go(string directions)
+        {
+            var directionArray = DirectionParser.Parse(directions);
+            Go(directionArray);
         }
     }
 }
